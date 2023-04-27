37f7dd42b7b915813774b51671b0e7558ef89276
LAPTOP-SC9RDVNE\domon <domonjianna@gmail.com>
2023-04-27 21:14:02 +0200

    Fixed and chainged colors of login and register layout

--- app/src/main/java/com/example/slagalica/MainActivity.java
+++ app/src/main/java/com/example/slagalica/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
