3aa8e5c348cae3fc1d713eef5edd04f5bb443ac9
LAPTOP-SC9RDVNE\domon <domonjianna@gmail.com>
2023-04-28 09:22:30 +0200

    Menu is opening new fragments

--- app/src/main/java/com/example/slagalica/MainActivity.java
+++ app/src/main/java/com/example/slagalica/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.slagalica;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -12,10 +14,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.slagalica.login_registration.RegistrationLoginActivity;
+import com.example.slagalica.menu.FriendsFragment;
 import com.example.slagalica.menu.HomeFragment;
-import com.example.slagalica.menu.LogoutFragment;
+import com.example.slagalica.menu.NotificationFragment;
+import com.example.slagalica.menu.ProfileFragment;
+import com.example.slagalica.menu.RangFragment;
 import com.example.slagalica.menu.SettingsFragment;
 import com.google.android.material.navigation.NavigationView;
 
@@ -61,23 +67,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace
+                        (R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            case R.id.menu_profile:
+                getSupportFragmentManager().beginTransaction().replace
+                        (R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+            case R.id.menu_notification:
+                getSupportFragmentManager().beginTransaction().replace
+                        (R.id.fragment_container, new NotificationFragment()).commit();
+                break;
+            case R.id.menu_friends:
+                getSupportFragmentManager().beginTransaction().replace
+                        (R.id.fragment_container, new FriendsFragment()).commit();
+                break;
+            case R.id.menu_rang:
+                getSupportFragmentManager().beginTransaction().replace
+                        (R.id.fragment_container, new RangFragment()).commit();
                 break;
             case R.id.menu_settings:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace
+                        (R.id.fragment_container, new SettingsFragment()).commit();
                 break;
             case R.id.menu_logout:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LogoutFragment()).commit();
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
                 break;
-
         }
+
         drawerLayout.closeDrawer(GravityCompat.START);
+
         return true;
     }
+
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
