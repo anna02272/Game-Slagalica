510f2633d04699d7947ff893cc3973cdd037452a
LAPTOP-SC9RDVNE\domon <domonjianna@gmail.com>
2023-06-06 20:01:35 +0200

    finished game korak po korak but without points

--- app/src/main/java/com/example/slagalica/games/KorakPoKorakActivity.java
+++ app/src/main/java/com/example/slagalica/games/KorakPoKorakActivity.java
@@ -1,14 +1,26 @@
 package com.example.slagalica.games;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -23,16 +35,24 @@ import java.util.Random;
 import com.example.slagalica.R;
 
 public class KorakPoKorakActivity extends AppCompatActivity {
-
     private List<Button> buttons;
     private FirebaseDatabase firebaseDatabase;
     private Map<Button, String> buttonSteps;
     private Random random;
     private CountDownTimer countDownTimer;
     private int currentCount = 7;
-    private String currentStepAnswer;
-    private EditText input;
 
+    private String answer;
+    private EditText input;
+    private Handler buttonHandler;
+    private Runnable buttonRunnable;
+    private int currentEnabledButtonIndex = 0;
+    private int currentButtonIndex = 1;
+
+    @Override
+    public void onBackPressed() {
+        return;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +60,7 @@ public class KorakPoKorakActivity extends AppCompatActivity {
         setContentView(R.layout.game_activity_korak_po_korak);
 
         PlayersFragment playersFragment = PlayersFragment.newInstance(71);
+        playersFragment.setGameType("KorakPoKorak");
 
         getSupportFragmentManager()
                 .beginTransaction()
@@ -57,6 +78,8 @@ public class KorakPoKorakActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
         Button confirmButton = findViewById(R.id.confirm);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,6 +111,33 @@ public class KorakPoKorakActivity extends AppCompatActivity {
         buttonSteps = new HashMap<>();
 
         retrieveSteps();
+
+        for (int i = 1; i <= 6; i++) {
+            Button button = buttons.get(i);
+            button.setEnabled(false);
+        }
+        buttonHandler = new Handler();
+        buttonRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Button button = buttons.get(currentButtonIndex);
+                button.setEnabled(true);
+                currentButtonIndex++;
+
+                Button button1 = buttons.get(currentEnabledButtonIndex);
+                String step = buttonSteps.get(button1);
+                if (step != null) {
+                    button1.setText(step);
+                    button1.setEnabled(false);
+                    currentEnabledButtonIndex++;
+                }
+
+                if (currentButtonIndex <= 7) {
+                    buttonHandler.postDelayed(this, 10000);
+                }
+            }
+        };
+
     }
 
     private void retrieveSteps() {
@@ -120,6 +170,7 @@ public class KorakPoKorakActivity extends AppCompatActivity {
 
     private void retrieveStep(final DataSnapshot stepSnapshot) {
         final String stepKey = stepSnapshot.getKey();
+        currentEnabledButtonIndex = 0;
         firebaseDatabase.getReference("korak_po_korak/" + stepKey).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -133,11 +184,9 @@ public class KorakPoKorakActivity extends AppCompatActivity {
                                 Button button = buttons.get(buttonIndex);
                                 buttonSteps.put(button, step);
                                 setButtonListener(button);
-                                if (stepKey.equals("steps" + (currentCount ))) {
-                                    currentStepAnswer = stepsMap.get("answer");
-                                }
                             }
                         }
+                        answer = stepsMap.get("answer");
                     }
                 }
             }
@@ -148,17 +197,41 @@ public class KorakPoKorakActivity extends AppCompatActivity {
             }
         });
     }
-
-
     private void checkAnswer() {
         String userInput = input.getText().toString().trim();
 
-        if (userInput.equalsIgnoreCase(currentStepAnswer)) {
-            Toast.makeText(KorakPoKorakActivity.this, "Correct answer!", Toast.LENGTH_SHORT).show();
+        if (answer != null && userInput.equalsIgnoreCase(answer)) {
+            Toast.makeText(KorakPoKorakActivity.this, "Tacan odgovor!", Toast.LENGTH_SHORT).show();
+            for (Button button : buttons) {
+                String step = buttonSteps.get(button);
+                if (step != null) {
+                    button.setText(step);
+                    button.setEnabled(false);
+                }
+            }
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+
+            if (countDownTimer != null) {
+                countDownTimer.cancel();
+            }
+
+            Toast.makeText(KorakPoKorakActivity.this, "Sledi igra MOJ BROJ!", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                   Intent intent = new Intent(KorakPoKorakActivity.this, MojBrojActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 5000);
         } else {
-            Toast.makeText(KorakPoKorakActivity.this, "Incorrect answer!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(KorakPoKorakActivity.this, "Netacan odgovor!", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 
 
     private void setButtonListener(final Button button) {
@@ -175,11 +248,12 @@ public class KorakPoKorakActivity extends AppCompatActivity {
     }
 
     private void startTimer() {
-        countDownTimer = new CountDownTimer(70000, 10000) {
+        countDownTimer = new CountDownTimer(71000, 10000) {
+            private Context context = KorakPoKorakActivity.this.getApplicationContext();
             @Override
             public void onTick(long millisUntilFinished) {
                 int time = (int) (millisUntilFinished / 1000);
-                int buttonIndex = currentCount ;
+                int buttonIndex = currentCount;
                 if (buttonIndex >= 7 && buttonIndex < buttons.size()) {
                     Button button = buttons.get(buttonIndex);
                     button.setText(String.valueOf(time));
@@ -189,6 +263,10 @@ public class KorakPoKorakActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
+                Toast.makeText(KorakPoKorakActivity.this, "Vase vreme je isteklo, sledi igra MOJ BROJ!",
+                        Toast.LENGTH_SHORT).show(); ;
+                Intent intent = new Intent(KorakPoKorakActivity.this, MojBrojActivity.class);
+                startActivity(intent);
 
             }
         };
@@ -196,15 +274,18 @@ public class KorakPoKorakActivity extends AppCompatActivity {
         countDownTimer.start();
     }
 
+
     @Override
     protected void onResume() {
         super.onResume();
         startTimer();
+        buttonHandler.postDelayed(buttonRunnable, 10000);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        buttonHandler.removeCallbacks(buttonRunnable);
         if (countDownTimer != null) {
             countDownTimer.cancel();
         }
